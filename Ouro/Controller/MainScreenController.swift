6d41f3c30726af5d5cc2a377e1b568851ebc627e
Jack Zhang <jackf.zh@gmail.com>
2019-04-28 18:50:45 -0400

    Included Victoria

--- Ouro/Controller/MainScreenController.swift
+++ Ouro/Controller/MainScreenController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainScreenController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        return .lightContent
     }
 
 
